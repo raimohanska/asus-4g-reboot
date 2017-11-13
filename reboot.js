71ac1b8cbb2943c6baae267c81079125b8cdb8ea
Juha Paananen <juha.paananen@gmail.com>
2017-11-13 23:07:39 +0200

    add a really nasty way to get server uptime in seconds

--- reboot.js
+++ reboot.js
@@ -1,32 +1,6 @@
-loginToken = process.env.logintoken
-if (!loginToken) {
-  throw new Error('logintoken environment variable missing')
-}
-
-loginUrl = 'http://192.168.1.1/login.cgi'
+const login = require('./login')
 
 request = require('request').defaults({jar: true});
-
-let login = callback => { request({
-  method: 'POST',
-  uri: loginUrl,
-  headers: {
-    'Referer': 'http://192.168.1.1/Main_Login.asp'
-  },
-  form: {
-    login_authorization: loginToken
-  }
-}, (error, response, body) => {
-  if (error) {
-    console.log('error', error)
-  } else if (response.headers['set-cookie']) {
-    callback()
-  } else {
-    console.log('login probably failed')
-    console.log(response.statusCode)
-    console.log('response headers', response.headers)
-  }
-})}
 
 let reboot = () => { 
   request({
---
+++ uptime.js
@@ -0,0 +1,21 @@
+const login = require('./login')
+
+
+request = require('request').defaults({jar: true});
+
+let uptime = () => { 
+  request({
+    uri: 'http://192.168.1.1/ajax_status.xml?hash=0.7616325189721338'
+  }, (error, response, body) => {
+    if (error) {
+      console.log('error', error)
+    } else {
+      let uptime = body.substring(body.indexOf('uptimeStr'))
+      uptime = uptime.substring(uptime.indexOf('(') + 1)
+      let uptimeSecs = uptime.split(' ')[0]
+      console.log(uptimeSecs)
+    }
+  })
+}
+
+login(uptime)
